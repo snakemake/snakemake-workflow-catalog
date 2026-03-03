1c7a5d90f9c8d1b7f70223ef097ef0684c092ff8
Johannes Köster <johannes.koester@uni-due.de>
2026-03-03 11:56:35 +0100

    Update redirect logic to also accept 'wf' parameter

--- source/_static/redirect.js
+++ source/_static/redirect.js
@@ -1,14 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   const params = new URLSearchParams(window.location.search);
-  const usage = params.get("usage");
+  let workflow = params.get("wf") || params.get("usage");
 
-  if (usage) {
+  if (workflow) {
     // Dynamically construct the target URL
-    const targetUrl = `docs/workflows/${usage}.html`;
+    const targetUrl = `docs/workflows/${workflow}.html`;
 
     // Redirect to the dynamically constructed URL
     window.location.href = targetUrl;
-  } else {
-    console.error("No usage parameter found in the URL.");
   }
 });
